2e6f7b1d92839877f7855de791699716b7b3d885
yishay <yishay.shlichter@gmail.com>
2022-01-28 13:37:31 +0200

    add interfaces
    Changes player and renderer class names to HumanPlayer and ConsoleRenderer respectively.
    Adding interfaces called Player and Renderer.

--- src/tictactoe/Player.java
+++ src/tictactoe/Player.java
@@ -1,38 +1,10 @@
 package tictactoe;
 
-public class Player {
-	private int countWin;
+public interface Player {
 
-	public Player() {
-		this.countWin = 0;
-	}
+	public void playTurn(Mark mark, Board board);
 
-	public void playTurn(Mark mark, Board board) {
-		int row, col;
-		do {
-			System.out.println("Player" + mark + " turn: enter location: ");
-			int loc = Game.scanner.nextInt();
-			if (loc < 99) {
-				col = (loc % 10) - 1;
-				row = (loc / 10 % 10) - 1;
-			} else
-				col = row = -1;
+	public int getCountWin();
 
-		} while (!board.putMark(mark, row, col));
-	}
-
-	/**
-	 * @return the countWin
-	 */
-	public int getCountWin() {
-		return countWin;
-	}
-
-	/**
-	 * @param countWin the countWin to set
-	 */
-	public void setCountWin() {
-		this.countWin++;
-	}
-
+	public void setCountWin();
 }
